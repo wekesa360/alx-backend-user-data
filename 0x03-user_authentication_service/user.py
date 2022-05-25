7816396b4897f9ab6fda1cfb42cec7774eae5083
Michael Wekesa <wekesa360@outlook.com>
2022-05-25 23:57:01 +0300

    add user.py

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,23 @@
+#!/usr/env python3
+"""
+a python user module
+"""
+
+from ast import Str
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class User(Base):
+    """
+    user model
+    """
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
+
+
